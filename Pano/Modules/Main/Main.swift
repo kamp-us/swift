35dac5467e67e09e4fc65e193254bb4a95066047
Mert Ahmet Gunes <mertahmetgunes@gmail.com>
2023-07-10 15:48:02 +0200

    Developed home view with link previews

--- Pano/Modules/Main/Main.swift
+++ Pano/Modules/Main/Main.swift
@@ -1,11 +1,10 @@
 import SwiftUI
-import ComposableArchitecture
 
 @main
 struct Main: App {
     var body: some Scene {
         WindowGroup {
-            RootView()
+            HomeView(items: PanoItem.sampleData)
         }
     }
 }
