de7a02ac36d1f8dcd4048695912420f5e938c4f4
Serdar Onur Karadağ <98060048+soKaradag@users.noreply.github.com>
2023-09-15 13:32:43 +0300

    Created AddPost View
    Created addPostView with dummy addPost functions and simple ui design.

--- Pano/Views/TabViews/AddPostView.swift
+++ Pano/Views/TabViews/AddPostView.swift
@@ -8,15 +8,69 @@
 import SwiftUI
 
 struct AddPostView: View {
-    @State private var title: String = "Sample Title But a Little Bit Long"
-    @State private var url: String = "https://example.com"
-    @State private var authorName: String = "John Doe"
-    @State private var numberOfComments: Int = 5
-    @State private var createAtText: String = "July 9, 2023"
+    //UserDefaults has userId and username. Also has userPraferences.
+    @EnvironmentObject var userDefaults: UserDefaults
+    
+    //Post Informations
+    @State private var title: String = ""
+    @State private var url: String = ""
+    @State private var content: String = ""
+    @State private var site: String = ""
+    @State private var slug: String = ""
+    
     
     var body: some View {
-            VStack {
-                Text("Add post view")
+        //This part opens in sheet. For navigationTitle we need navigationStack.
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Title", text: $title)
+                    TextField("Content", text: $content, axis: .vertical)
+                        .lineLimit(6, reservesSpace: true)
+                } header: {
+                    Text("POST DETAILS")
+                }
+                
+                Section {
+                    TextField("URL", text: $url)
+                    TextField("Site", text: $site)
+                    TextField("Slug", text: $slug)
+                } header: {
+                    Text("URL DETAILS")
+                }
+                
+                HStack {
+                    Spacer()
+                    Button {
+                        //In Form button action is not working as expected. For the solution i use onTapGasture.
+                    } label: {
+                        Text("Add Post")
+                    }
+                    //Use button action here.
+                    .onTapGesture {
+                        addPost()
+                    }
+                    Spacer()
+                }
+
+                
             }
+            .navigationTitle("Add Post")
+        }
     }
+    
+    //Dummy addPost func
+    private func addPost() {
+        //Fetch currentUser information from database before add post.
+        let currentUser: User = User(id: "1", name: "FelfecirBoy", emailVerified: Date(), accounts: [], sessions: [], posts: [], comments: [], commentUpvotes: [], upvotes: [], createdAt: Date(), updatedAt: Date(), deletedAt: Date(), notifications: [], notifies: [])
+        
+        //Call addPost func from viewModel and use currentUser for user information.
+        //This is dummyFunc too.
+        func calledAddPost() {
+            print(currentUser.name ?? "Unkown")
+            print("Added Post")
+        }
+    }
+    
 }
+
